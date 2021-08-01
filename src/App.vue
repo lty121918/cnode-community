8ecb31b44727be25e56a577f5090987c67d9951d
Your Name <1219186777@qq.com>
2021-08-01 23:37:17 +0800

    8.1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div id="app">  
       <navbar></navbar>
       <!-- <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> -->
-    </div>
     <!-- <router-view/> -->
   </div>
 </template>
--- src/components/Common/navbar/index.vue
+++ src/components/Common/navbar/index.vue
@@ -2,27 +2,20 @@
   <div class="navbar">
   <div class="navbar-inner">
     <div class="container">
-      <a class="brand" href="/">
-        
+      <a class="brand" href="/">     
           <img src="//static2.cnodejs.org/public/images/cnodejs_light.svg">
-        
       </a>
 
       <form id="search_form" class="navbar-search" action="/search">
-        <input type="text" id="q" name="q" class="search-query span3" value="">
+        <input type="text"  class="search-query span3" value="">
       </form>
       <ul class="nav pull-right">
         <li><a href="/">首页</a></li>
-        
         <li><a href="/getstart">新手入门</a></li>
         <li><a href="/api">API</a></li>
-        
         <li><a href="/about" target="">关于</a></li>
-        
-        
         <li><a href="/signup">注册</a></li>
-        <li><a href="/signin">登录</a></li>
-        
+        <li><a href="/signin">登录</a></li>  
       </ul>
       <a class="btn btn-navbar" id="responsive-sidebar-trigger">
         <span class="icon-bar"></span>
@@ -40,6 +33,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped src="./index.less">
+
 
 </style>
