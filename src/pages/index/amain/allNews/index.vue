3b0afe0a1c0b072ef11a46f44d3ea4567e7383d8
Your Name <1219186777@qq.com>
2021-08-07 22:51:19 +0800

    8.7

--- src/pages/index/amain/allNews/index.vue
+++ src/pages/index/amain/allNews/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="allNews">
     <div class="nav-list" v-for="(item,index) in listData" >
-      <!-- v-show="item.good==true" -->
       <img :src=item.author.avatar_url alt="">
       <div class="count">
           <span class="one">{{item.reply_count}}</span>
@@ -13,6 +12,7 @@
       <div class="putTab" v-show="item.good==false&&item.top==false&&item.tab=='share'">分享</div>
       <div class="putTab" v-show="item.good==false&&item.top==false&&item.tab=='ask'">问答</div>
       <div class="putTab" v-show="item.good==false&&item.top==false&&item.tab=='job'">招聘</div>
+      <!-- 标题，路由跳转到详情页 -->
       <router-link :to="`/topic/${item.id}`" class="title"> {{item.title}}</router-link> 
       <div class="create_at">{{item.create_at}}</div>
     </div>
@@ -22,15 +22,9 @@
 <script>
 export default {
 name: 'AllNews',
-data(){
-    return{
-      news:[],
-      domIndex:1
-    }
-},
+
 props:["listData"],
-mounted(){
-}
+
 }
 </script>
 
