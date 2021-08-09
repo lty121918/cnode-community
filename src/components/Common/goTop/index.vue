a42976f66085248baed09c78e9f23bdedfee715f
Your Name <1219186777@qq.com>
2021-08-09 09:38:35 +0800

    8.9

---
+++ src/components/Common/goTop/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="gotop">
+      <span>回到顶部</span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang="less" scoped>
+@import "./index.less";
+</style>
